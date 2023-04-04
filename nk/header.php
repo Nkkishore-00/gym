99de40a1a83ae467c9b9cb3695882978b6746c92
Nkkishore-00 <kingmakernandhakishore@gmail.com>
2023-04-04 14:45:19 +0530

    Update header.php

--- nk/header.php
+++ nk/header.php
@@ -9,6 +9,9 @@
       <li class="nav-item active">
         <a class="nav-link" href="dashboard.php">Home <span class="sr-only">(current)</span></a>
       </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">Home <span class="sr-only">(current)</span></a>
+      </li>
       
       
       
